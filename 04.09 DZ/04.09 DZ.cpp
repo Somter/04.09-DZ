9fddc3d6e896de47f5b6f8a721a825ccde897f36
Sasha <alecsand.unit@gmail.com>
2023-09-05 14:29:49 +0300

    сделал 6 пунктов из 8

--- 04.09 DZ/04.09 DZ.cpp
+++ 04.09 DZ/04.09 DZ.cpp
@@ -2,10 +2,215 @@
 //
 
 #include <iostream>
+#include <fstream>	
+using namespace std;	
+#define YELLOW_TEXT "\033[33m"	
+#define RESET_COLOR "\033[0m"
+#define RED_TEXT "\033[31m"
+#define GREEN_TEXT "\033[32m"	
+#define PROBEL cout << endl;cout << "--------------" << endl;cout << endl;
 
-int main()
+const int SIZE = 15;	
+const int MAX_EMPLOYEES = 50;	
+struct Person {
+	char* surname;
+	char* name;
+	char* number;
+	double salary;			
+};
+void Data_Input(Person* Employee, int &count)		
 {
-    std::cout << "Hello World!\n";
+	Person& employee = Employee[count];
+	employee.surname = new char[SIZE];
+	cout << "Ввведите фамилию: " ;
+	cin >> employee.surname;
+
+	employee.name = new char[SIZE];
+	cout << "Введите имя: ";
+	cin >> employee.name;
+
+	employee.number = new char[SIZE];	
+	cout << "Введите номер телефона: " << endl;		
+	cout << YELLOW_TEXT << "Пример: (063)5635904 " << RESET_COLOR;	
+	int i = 0;
+	do {
+		i++;
+		if (i > 1) {
+			cout << RED_TEXT << "Некорректный номер телефона" << RESET_COLOR;
+		}
+		cout << "\nВвод: ";
+		cin >> employee.number;
+	} while ((employee.number[0] != '(') || (employee.number[4] != ')'));
+
+	cout << "Введите размер заработной платы: ";
+	cin >> employee.salary;	
+	count++;	
+}
+void Delete_Employee(Person* Employee, int& count) 
+{
+	int Delete_choice;
+	char Yes_Or_No[5];	
+	cout << "Количество сотрудников: " << count << endl;
+	cout << "Какого удалить: ";
+	cin >> Delete_choice;
+	cout << "Вы действительно хотите удалить этого сотрудника? Yes/No: ";
+	cin >> Yes_Or_No;	
+	if(strcmp(Yes_Or_No, "yes") == 0 || strcmp(Yes_Or_No, "Yes") == 0){
+		if (Delete_choice <= count && Delete_choice > 0) {
+			for (int i = Delete_choice - 1; i < count - 1; i++) {
+				Employee[i] = Employee[i + 1];
+			}
+			cout << GREEN_TEXT << "Сотрудник успешно удалён!" << RESET_COLOR << endl;	
+			count--;	
+		}
+		else {
+			cout << RED_TEXT << "Некорректный выбор для удаления." << RESET_COLOR << endl;
+		}	
+	}
+	else if (strcmp(Yes_Or_No, "no") == 0 || strcmp(Yes_Or_No, "No") == 0) {
+		cout << GREEN_TEXT << "Удаление прекращено!" << RESET_COLOR << endl;	
+		return;
+	}		
+	else {
+		cout << RED_TEXT << "Некорректный выбор" << RESET_COLOR << endl;	
+	}
+
+}
+void SortingSurname(Person* Employee, int& count) 
+{
+	for (int i = 0; i < count - 1; i++) {
+		for (int j = 0; j < count - i - 1; j++) {
+			if (strcmp(Employee[j].surname, Employee[j + 1].surname) > 0) {
+				Person temp = Employee[j];
+				Employee[j] = Employee[j + 1];
+				Employee[j + 1] = temp;
+			}
+		}
+	}
+}
+void SearchSurname(Person* Employee, int& count)			
+{	
+	char YourSurname[20];	
+	cout << "Введите фамилию которую нужно найти: ";
+	cin >> YourSurname;	
+	bool found = false; 
+	for (int i = 0; i < count; i++) {
+		if (strcmp(YourSurname, Employee[i].surname) == 0) {
+			cout << GREEN_TEXT << "Такой сотрудник есть!" << RESET_COLOR << endl;	
+			cout << "Фамилия: ";	
+			cout << Employee[i].surname << endl;	
+			cout << "Имя: ";
+			cout << Employee[i].name << endl;
+			cout << "Номер: ";
+			cout << Employee[i].number << endl;	
+			cout << "Зарплата: ";
+			cout << Employee[i].salary << endl;	
+			found = true; 
+		}
+	}
+
+	if (!found) {
+		cout << RED_TEXT << "Сотрудник с фамилией " << RESET_COLOR << YourSurname << RED_TEXT <<  " не найден." << RESET_COLOR << endl;	
+	}	
+}
+void SalarySearch(Person* Employee, int& count) 
+{
+	double start, end;	
+	cout << "Введите диапазон зарплаты " << endl;	
+	cout << "Ввведите начало диапазона: ";
+	cin >> start;
+	cout << "Введите конец дипазона: ";
+	cin >> end;	
+	for (int i = 0; i < count; i++) {
+		if (Employee[i].salary >= start && Employee[i].salary <= end) 
+		{
+			cout << endl;	
+			cout << GREEN_TEXT << "Сотрудник с такой зарплатой есть! " << RESET_COLOR << endl;	
+			cout << "Фамилия: ";
+			cout << Employee[i].surname << endl;
+			cout << "Имя: ";
+			cout << Employee[i].name << endl;
+			cout << "Номер: ";
+			cout << Employee[i].number << endl;
+			cout << "Зарплата: ";
+			cout << Employee[i].salary << endl;	
+		}	
+		else {
+			cout << RED_TEXT << "Сотрудников с такой зарплатой нет! " << RESET_COLOR << endl;	
+		}
+	}
+
+}
+
+void Print(Person* Employee) 
+{
+	cout << "Фамилия: ";
+	cout << Employee->surname << endl;	
+	cout << "Имя: ";
+	cout << Employee->name << endl;	
+	cout << "Номер: ";
+	cout << Employee->number << endl;	
+	cout << "Зарплата: ";
+	cout << Employee->salary << endl;		
+}
+
+int main()	
+{
+	setlocale(LC_ALL, "");
+	Person Employee[MAX_EMPLOYEES];		
+	int choise;	
+	int count = 0;		
+	do {
+		cout << "Выбирите действие: " << endl;
+		cout << "1 - добавление нового сотрудника" << endl;
+		cout << "2 - демонстрация содеожимого всей книги на экран" << endl;
+		cout << "3 - удаление сотрудника по выбору" << endl;
+		cout << "4 - сортировка по фамилии" << endl;	
+		cout << "5 - поиск по фамилии и демонстрация результатов на экран" << endl;
+		cout << "6 - поиск по диапазону зароботной платы и демонстрация результатов на экран" << endl;
+		cout << "7 - загрузка информации из файла" << endl;
+		cout << "8 - запись информации в файл" << endl;
+		cout << "9 - выход" << endl;
+		cout << "Ваш выбор: ";
+		cin >> choise;
+		system("cls");
+		switch (choise) {
+		case 1:
+			Data_Input(Employee, count);
+			PROBEL
+			break;
+		case 2:
+			for (int i = 0; i < count; i++) {
+				Print(Employee+i);		
+				PROBEL	
+			}
+			break;	
+		case 3:
+			Delete_Employee(Employee, count);
+			PROBEL
+			break;
+		case 4:
+			SortingSurname(Employee, count);	
+			cout << GREEN_TEXT << "Сортиовка заверешена! " << RESET_COLOR << endl;	
+			PROBEL	
+			break;	
+		case 5:
+			SearchSurname(Employee, count);	
+			PROBEL
+			break;
+		case 6:
+			SalarySearch(Employee, count);
+			PROBEL	
+			break;
+		case 7:
+		
+			break;
+		case 8:
+		
+			PROBEL
+			break;
+		}	
+	} while (choise != 9);	
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
